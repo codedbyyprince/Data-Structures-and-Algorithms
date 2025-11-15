d7dce6cc4d8672739511085d44625b557b13cfb2
prince <princenagda35@gmail.com>
2025-11-15 19:47:37 +0530

    1 question morre

--- 02_Arrays/medium/spiral_manner_matrix.py
+++ 02_Arrays/medium/spiral_manner_matrix.py
@@ -1,2 +1,31 @@
-def spiral_manner_matrix(matrix):
+def spring_manner_matrix(matrix):
+    m = len(matrix)
+    n = len(matrix[0])
+    top = 0
+    bottom = m - 1
+    left = 0
+    right = n - 1
+    ans = []
     
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            ans.append(matrix[top][i])
+        top += 1
+        
+        for i in range(top, bottom + 1):
+            ans.append(matrix[i][right])
+        right -= 1
+        
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                ans.append(matrix[bottom][i])
+            bottom -= 1
+        
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                ans.append(matrix[i][left])
+            left += 1
+    
+    return ans
+matrix = [[1,2,3],[4,5,6],[7,8,9],[9,8,7],[6,5,4],[3,2,1]]
+print(spring_manner_matrix(matrix))
