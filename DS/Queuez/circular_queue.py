16610e9bc966dec40bacbc5a1ff029c3d4ca3d0d
prince <princenagda35@gmail.com>
2025-12-17 21:16:45 +0530

    dequeue learned

--- DS/Queuez/circular_queue.py
+++ DS/Queuez/circular_queue.py
@@ -7,8 +7,16 @@ class MyQueue:
 
     def enqueue(self, x):
         if self.size == self.capacity:
-            return "Queue is full"
+            print("Queue is full")
         rear = (self.front + self.size) % self.capacity
         self.arr[rear] = x
         self.size += 1
         return self.arr 
+    
+    def dequeue(self, x):
+        if self.size == 0:
+            print('Queue is empty ')
+            return - 1
+        res = self.arr[self.front]
+        self.front = (self.front + 1 )% self.capacity
+        return res 
