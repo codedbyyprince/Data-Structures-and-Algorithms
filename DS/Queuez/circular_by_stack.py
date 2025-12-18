72bec365705cf564d3a4ecafca84fb36c43860db
prince <princenagda35@gmail.com>
2025-12-18 21:51:40 +0530

    queue by stack approach en costly

---
+++ DS/Queuez/circular_by_stack.py
@@ -0,0 +1,34 @@
+# approach enqueue costly
+class queue_by_stack_en_costly:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def enqueue(self, x):
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        
+        self.s1.append(x)
+
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def dequeue(self):
+        if not self.s1:
+            return 
+        self.s1.pop()
+    
+    def front(self):
+        if not self.s1:
+            return 
+        return self.s1[-1]
+
+
+# approach dequeue costly
+class queue_by_stack_de_costly:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def enqueue(self, x):
+        pass
