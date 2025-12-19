a4be331d3ea4de01df406b9af06e933c488be05f
prince <princenagda35@gmail.com>
2025-12-19 19:23:51 +0530

    queue completed

--- DS/Queuez/circular_by_stack.py
+++ DS/Queuez/circular_by_stack.py
@@ -31,4 +31,22 @@ class queue_by_stack_de_costly:
         self.s2 = []
 
     def enqueue(self, x):
-        pass
+        self.s1.append(x)
+
+    def dequeu(self):
+        if not self.s1 and not self.s2:
+            return 
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+
+        self.s2.pop()
+
+    def get_front(self):
+        if self.s2:
+            return self.s2[-1]
+        elif self.s1:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+            return self.s2[-1]
+        return -1 
