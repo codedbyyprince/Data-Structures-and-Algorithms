bd86cbcfbed875e057e966fc091afea667bc93b3
prince <princenagda35@gmail.com>
2025-12-31 16:35:36 +0530

    inorder transversal

---
+++ DS/NON-LINEAR/binary_traversal.py
@@ -0,0 +1,35 @@
+class node: 
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.data = key 
+
+# Initializing and allocating memory for tree nodes
+first_node = node(10)
+sec_node = node(20)
+third_node = node(30)
+forth_node = node(40)
+fifth_node = node(80)
+six_node = node(60)
+seventh_node = node(90)
+
+# creating nodes 
+first_node.right = sec_node
+first_node.left = third_node
+
+third_node.left = six_node
+third_node.right = seventh_node
+
+sec_node.left = forth_node
+sec_node.right = fifth_node
+
+
+def inorder(node):
+    if node is None:
+        return
+    inorder(node.left)
+    print(node.data)
+    inorder(node.right)
+
+
+inorder(first_node)
