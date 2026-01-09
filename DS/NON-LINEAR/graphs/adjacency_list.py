70e676b7b4e8cd6a150c4bccd39be16cce9e88d2
prince <princenagda35@gmail.com>
2026-01-09 18:58:43 +0530

    adjacency list  y list graph implementation completed

---
+++ DS/NON-LINEAR/graphs/adjacency_list.py
@@ -0,0 +1,24 @@
+def adjacency_list_undirected(V, edges):
+    adjacency_list = {}
+
+    for i in range(V):
+        adjacency_list[i] = []
+
+    for u, v in edges:
+        adjacency_list[u].append(v)
+        adjacency_list[v].append(u)
+
+    for vertex in adjacency_list:
+        print(vertex, "->", adjacency_list[vertex])
+
+V = 6
+edges = [
+    [0,1],[0,2],[0,4],[0,5],
+    [1,3],[1,5],[1,2],
+    [2,4],[2,5],
+    [3,4],[3,5],
+    [4,3],[4,1],
+    [5,3],[5,4]
+]
+
+adjacency_list_undirected(V, edges)
