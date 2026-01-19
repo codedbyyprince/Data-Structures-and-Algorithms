acc29cd604ee33b8cbb09dcfc700e993768c5f1d
prince <princenagda35@gmail.com>
2026-01-19 07:03:59 +0530

    disconnected bfs completed

--- DS/NON-LINEAR/graphs/bfs.py
+++ DS/NON-LINEAR/graphs/bfs.py
@@ -13,8 +13,10 @@ def dict_graph(V , edges):
         adj[v].append(u)
     
     return adj
+# BFS 
 
-def bfs(adj, src):
+# Connected 
+def Connected_bfs(adj, src):
     visited = set()
     q = deque()
     res = []
@@ -33,15 +35,52 @@ def bfs(adj, src):
     
     return res
 
+# Disconnected func
+
+# Helper
+def bfs_from_node(adj, src , res , visited):
+    q = deque()
+
+    visited.add(src)
+    q.append(src)
+
+    while q:
+        curr = q.popleft()
+        res.append(curr)
+
+        for neighbour in adj[curr]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                q.append(neighbour)
+    
+# Disconnected
+def  bfs_disconnected(adj):
+    visited = set()
+    res = []
+
+    for node in adj:
+        if node not in visited:
+            bfs_from_node(adj, node, res, visited)
+    
+    return res
+
+
+# Example run
+
 V = 6
 edges = [
-    [0,1],[0,2],[0,4],[0,5],
-    [1,3],[1,5],[1,2],
-    [2,4],[2,5],
-    [3,4],[3,5],
-    [4,1],[5,4]
+    [0,1],[0,2],[0,3],
+    [1,2],[1,3],
+    [2,3],
+
+    [4,5]
 ]
+
 
 adj = dict_graph(V, edges)
 
-print(adj)
+print(f'Graph = {adj}')
+
+print(f'Connected BFS = {Connected_bfs(adj, 0)}')
+
+print(f'Disconnected BFS = {bfs_disconnected(adj)}')
